f74cba4073e40d61572a457b6207427cb71bb203
makjesus <makjesus2008@gmail.com>
2024-07-22 10:46:59 -0300

    Four commit

--- app.rb
+++ app.rb
@@ -1,45 +1,7 @@
 # app.rb
 require 'sinatra'
-require 'sinatra/activerecord'
-require 'require_all'
+require '.lib/db'
 
-set :database_file, 'config/database.yml'
-
-require_all 'models'
-
-class App < Sinatra::Base
-  register Sinatra::ActiveRecordExtension
-
-
-  get '/' do
-  @items = Item.all
-  erb :index
+get '/' do
+  'Hello, world!'
 end
-
-get '/items/new' do
-  erb :new
-end
-
-post '/items' do
-  Item.create(params[:item])
-  redirect '/'
-end
-
-get '/items/:id/edit' do
-  @item = Item.find(params[:id])
-  erb :edit
-end
-
-put '/items/:id' do
-  item = Item.find(params[:id])
-  item.update(params[:item])
-  redirect '/'
-end
-
-delete '/items/:id' do
-  Item.find(params[:id]).destroy
-  redirect '/'
-end
-end
-
-:end
--- db/migrate.rb
+++ db/migrate.rb
@@ -3,8 +3,6 @@ class CreateItems < ActiveRecord::Migration[6.1]
     create_table :items do |t|
       t.string :name
       t.text :description
-      t.decimal :price, precision: 8, scale: 2
-
       t.timestamps
     end
   end
